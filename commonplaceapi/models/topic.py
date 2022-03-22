23737715395d7e0a063605859cc804f6d1497221
Emma Vogelmeier <emmameiervogel@gmail.com>
2022-03-21 20:48:47 -0500

    clean and comment code

--- commonplaceapi/models/topic.py
+++ commonplaceapi/models/topic.py
@@ -1,11 +1,10 @@
-from pyexpat import model
 from django.db import models
 from .entry import Entry
 from .commonplace_user import CommonplaceUser
 
 
 class Topic(models.Model):
-    """ Model for Entry Topics """
+    """Model for Entry Topics"""
 
     user = models.ForeignKey(CommonplaceUser, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
